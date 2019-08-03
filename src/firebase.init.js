19a952b8c83de3f81840a6379f54974090d89962
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-08-04 05:02:43 +0530

    added firebase and setup the config

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2_sHO4--eMTdP6sBlxQBtu97EZOCvZ5Y",
+  authDomain: "mitrmoji.firebaseapp.com",
+  databaseURL: "https://mitrmoji.firebaseio.com",
+  projectId: "mitrmoji",
+  storageBucket: "",
+  messagingSenderId: "751769084220",
+  appId: "1:751769084220:web:16cdcc317638a4e4"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
