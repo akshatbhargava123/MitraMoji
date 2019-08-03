3e4a33fbe8c31173627650d192b62bcf63aeb15b
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2019-08-03 17:22:53 +0530

    basic streaming now working, removed tests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ChannelInput from "./components/ChannelInput";
+import Call from "./components/Call";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      channel: ""
+    };
+  }
+
+  selectChannel = channel => {
+    this.setState({ channel });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <ChannelInput selectChannel={this.selectChannel} />
+        <Call channel={this.state.channel} />
+      </div>
+    );
+  }
 }
 
 export default App;
