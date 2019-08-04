7c31561c9bc8773288eae58d613ee4c0c1a0953a
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-08-04 07:37:09 +0530

    created file for maintaining languages

---
+++ src/assets/language.js
@@ -0,0 +1,22 @@
+export default {
+  english: {
+    EMAIL: 'Email',
+    PASSWORD: 'Password',
+    FORGET_PASSWORD: 'Forget Password',
+    LOGIN: 'LOGIN',
+    NEW: 'New',
+    SIGN_UP_NOW: 'SIGN UP NOW!',
+    START: 'START',
+    SUBMIT: 'SUBMIT'
+  },
+  hindi: {
+    EMAIL: 'ईमेल',
+    PASSWORD: 'पासवर्ड',
+    FORGET_PASSWORD: 'पासवर्ड भूल गए',
+    LOGIN: 'लॉग इन करें',
+    NEW: '',
+    SIGN_UP_NOW: 'अभी साइनअप करें!',
+    START: 'शुरु',
+    SUBMIT: 'जमा करें'
+  }
+}
