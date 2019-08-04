9d8fa17288385b51198bbf7da4f09fd240e93895
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-08-04 08:38:27 +0530

    added internalization in login, home, signup page completely

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,33 @@
 import React, { Component } from 'react';
 import './Home.css';
+import languages from '../assets/language'; 
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: {}
+      user: {},
+      language: {}
     };
   }
   componentDidMount() {
-		console.log(this.props.location.state);
-    this.setState({ user: this.props.location.state.user });
+    this.setState({ user: JSON.parse(localStorage.getItem('user') || {}) });
+    const langKey = localStorage.getItem('lang') || 'english';
+    this.setState({ language: languages[langKey] });
   }
   startGame = () => {
     this.props.history.push('/game');
   }
   render() {
+    const { language } = this.state;
     return (
       <div className="main">
 				<div style={{ textAlign: 'center' }}>
 					<img className="avatar-large" src={this.state.user.imageUrl} onClick={this.showUploadPanel} />
 				</div>
         <img src={this.state.user.photoURL} alt="avatar" className="avatar" width="100%" />
-        <h3 className="username">{this.state.user.displayName}</h3>
-        <button className="button" onClick={this.startGame}>Start</button>
+        <h2 className="username">{language.WELCOME} {this.state.user.displayName}</h2>
+        <button className="button" onClick={this.startGame}>{language.START}</button>
       </div>
     );
   }
