fc94c1b8d43c9b9924f8e52aa004a1f34afa3231
akshat <akshatbhargavadeepa@gmail.com>
2019-08-04 08:37:24 +0530

    matchmaking almost done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,70 @@
 import React, { Component } from 'react';
+import firebase from '../firebase.init';
 import './Home.css';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: {}
-    };
+			user: {},
+			findingMatch: false,
+			selfHosted: false
+		};
+
+		this.unsubscribe = null;
   }
   componentDidMount() {
 		console.log(this.props.location.state);
     this.setState({ user: this.props.location.state.user });
   }
   startGame = () => {
-    this.props.history.push('/game');
-  }
+		this.setState({ findingMatch: true });
+		const matchmakingCollection = firebase.firestore().collection('matchmaking');
+		this.unsubscribe = matchmakingCollection.onSnapshot((res) => {
+			const matches = res.docs.map(r => r.data());
+			let joined = false;
+			
+			// find and join someone else' hosted match
+			matches.forEach(match => {
+				if (match.status === 'FINDING') {
+					matchmakingCollection.doc(match.timestamp).update('status', 'RUNNING');
+					matchmakingCollection.doc(match.timestamp).update('player2', {
+						name: this.state.user.displayName,
+						uid: this.state.user.uid
+					});
+					joined = true;
+					console.log('MATCH FOUND', match);
+				}
+			});
+
+			// add a new match to queue if somebody else not already finding the match
+			if (!joined) {
+				this.unsubscribe();
+				this.setState({ selfHosted: true });
+				const newMatchTimestamp = Number(new Date()).toString();
+				matchmakingCollection.doc(newMatchTimestamp).set({
+					player1: {
+						name: this.state.user.displayName,
+						uid: this.state.user.uid
+					},
+					status: 'FINDING',
+					timestamp: newMatchTimestamp
+				});
+				matchmakingCollection.doc(newMatchTimestamp).onSnapshot(res => {
+					const match = res.data();
+					if (match.status === 'RUNNING') {
+						console.log('MATCH STARTED', match);
+					}
+				});
+			}
+		});
+    // this.props.history.push('/game');
+	}
+	
+	componentWillUnmount() {
+		if (this.unsubscribe) this.unsubscribe();
+	}
+
   render() {
     return (
       <div className="main">
@@ -23,7 +73,12 @@ class Home extends Component {
 				</div>
         <img src={this.state.user.photoURL} alt="avatar" className="avatar" width="100%" />
         <h3 className="username">{this.state.user.displayName}</h3>
-        <button className="button" onClick={this.startGame}>Start</button>
+        <button className="button" onClick={this.startGame}>
+				{
+					this.state.findingMatch ?
+					'Finding Match...' : 'Start Game'
+				}
+				</button>
       </div>
     );
   }
